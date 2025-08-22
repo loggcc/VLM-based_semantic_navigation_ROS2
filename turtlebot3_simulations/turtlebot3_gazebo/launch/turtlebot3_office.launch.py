abf2d9cc1d9789a83b0c3468ee484e9d095d4aea
Ariel Hu <bhu16300@gmail.com>
2025-08-22 12:58:53 +0700

    add models for office gazebo world

--- turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_office.launch.py
+++ turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_office.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     world = os.path.join(
         get_package_share_directory('turtlebot3_gazebo'),
         'worlds',
-        'office_4.world'  # office_world.world
+        'office.world'  # office_world.world
     )
 
     gzserver_cmd = IncludeLaunchDescription(
